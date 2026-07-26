d47ab53fc7bb90b645e2703263ad8da6a8b2bb42
Mushroomcowisheggs <egg_mushroomcow@foxmail.com>
2026-07-26 22:52:33 +0800

    Delete DungeonSlime2 directory

--- DungeonSlime2/Core/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-// Core/GlobalUsings.cs
-// This file provides global using directives for the core library. 
-global using MonoGameLibrary.Core.Concurrency;
-global using MonoGameLibrary.Core.Diagnostics;
-global using MonoGameLibrary.Core.Hosting;
-global using MonoGameLibrary.Core.Lifecycle;
-global using MonoGameLibrary.Core.Pooling;
-global using MonoGameLibrary.Core.Time;
